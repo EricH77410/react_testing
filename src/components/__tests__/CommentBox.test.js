b0351c96dcd39cdcae56d5b8e834e7fc3f049b6c
Eric Hamimi <PB10288@Int.CSDental.com>
2018-08-07 14:22:29 +0200

    before

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import CommentBox from '../CommentBox'
+import { mount } from 'enzyme';
 
-i('should has a textarea and a button', () => {
+let wrapped;
+
+beforeEach(()=>{
+  wrapped = mount(<CommentBox/>)
+});
+
+it('should has a textarea and a button', () => {
   
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+
 })
+
+afterEach(()=>{
+  wrapped.unmount();
+});
